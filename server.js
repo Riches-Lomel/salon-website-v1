d5c5f423873b17b3fbc66451e4083a0205114ce1
Riches lomel <153424556+Riches-Lomel@users.noreply.github.com>
2025-08-20 15:46:38 +0000

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,9 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(cors({
+  origin: "https://ultimatesalonlegon.netlify.app"
+}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
